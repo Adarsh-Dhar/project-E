9894d60c5f05e7a3738c3d36b564a1de91fefb71
Adarsh Dhar <dharadarsh0@gmail.com>
2024-11-08 16:46:29 +0530

    smart-contracts done

--- smart-contracts/contracts/interfaces/IUSDe.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-interface IUSDe {
-    function mint(address to, uint256 amount) external;
-    function burn(address from, uint256 amount) external;
-    function addReserveAsset(address asset) external;
-    function removeReserveAsset(address asset) external;
-} 
